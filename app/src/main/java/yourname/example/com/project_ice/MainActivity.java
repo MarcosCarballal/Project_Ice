e17734e14923732ea8a06e24be4d033be5c9586c
HypnotizedPlatypus <mcarball@seas.upenn.edu>
2017-07-09 11:25:18 -0700

    Implemented WeatherReport.java JSON call function. Need to design test for WeatherReport.java function as well as find a way to get the devices location in MainActivity.java

--- app/src/main/java/yourname/example/com/project_ice/MainActivity.java
+++ app/src/main/java/yourname/example/com/project_ice/MainActivity.java
@@ -3,11 +3,15 @@ package yourname.example.com.project_ice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import yourname.example.com.project_ice.WeatherReport.WeatherReport;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boolean debugging = true;
+        WeatherReport myReport = new WeatherReport(0,0, "TimeMachine", debugging);
     }
 }
--- app/src/main/java/yourname/example/com/project_ice/WeatherReport/WeatherReport.java
+++ app/src/main/java/yourname/example/com/project_ice/WeatherReport/WeatherReport.java
@@ -2,29 +2,41 @@ package yourname.example.com.project_ice.WeatherReport;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
-import java.net.URL;
+
 
 import yourname.example.com.project_ice.MainActivity;
 // This class is for the call and the parsing of the JSON object provided by the DarkSky API
 /**
  * Created by marcos on 7/8/2017.
- * Currently trying to mimick the behavior of http://www.studytrails.com/java/json/java-org-json/
+ * Currently trying to mimic the behavior of http://www.studytrails.com/java/json/java-org-json/
  */
 public class WeatherReport extends MainActivity {
-    public WeatherReport(int CoordX, int CoordY, String Method)
-    {
+    public WeatherReport(int CoordX, int CoordY, String Method, boolean debugging) { // Constructor
         if (Method.equals("TimeMachine"))
         {
             System.out.println("The called method was Time Machine");
-            String url = "https://api.darksky.net/forecast/f060c1a85d245405e10edd9d1a2b5ced/" + CoordX + "," +CoordY;
-            JSONObject json = new JSONObject(genreJson);
-            try (String result = json.get(url);){
-
-            }catch (JSONException e){
+            String url = "https://api.darksky.net/forecast/f060c1a85d245405e10edd9d1a2b5ced/"
+                    + CoordX + "," +CoordY; // Was told not to embed private key into Java code but
+                                            // can't quite find how to implement it otherwise.
+            JSONObject json = new JSONObject();
+            /*try(String result = json.get(url).toString()){
+            }catch (JSONException e) {
+                e.printStackTrace();
                 System.out.println("The JSON call failed");
+            }{
+
             }
 
+            */
+            JSONArray result = null;
+            try {
+                result = json.getJSONArray(url);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (debugging) {System.out.println(result);}
 
         } else if (Method.equals("Forecast"))
         {
